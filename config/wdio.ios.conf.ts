947ba32d6b3a2195b724ca469965e8a614d3a449
Djones-qa <your@email.com>
2026-03-23 13:47:26 -0400

    ci: add iOS GitHub Actions job with XCUITest driver

---
+++ config/wdio.ios.conf.ts
@@ -0,0 +1,14 @@
+﻿import { config as baseConfig } from './wdio.base.conf';
+
+export const config = {
+  ...baseConfig,
+  specs: ['./tests/**/*.spec.ts'],
+  capabilities: [{
+    platformName: 'iOS',
+    'appium:deviceName': 'iPhone 15',
+    'appium:platformVersion': '17.0',
+    'appium:automationName': 'XCUITest',
+    'appium:browserName': 'Safari',
+    'appium:noReset': false,
+  }],
+};
